860cdd0b1710d3b5d732ec0a7d42e3fa81fbf585
nvnYampati <nk.c360.ny@gmail.com>
2023-01-06 17:04:26 +0530

    Added Test Components

---
+++ force-app/main/default/lwc/testProductList/testProductList.js
@@ -0,0 +1,39 @@
+import { LightningElement, wire, api } from 'lwc';
+import getChef from '@salesforce/apex/ChefController.getChef';
+import Chef from '@salesforce/schema/Chef__c'
+import ChefName from '@salesforce/schema/Chef__c.Name'
+export default class TestProductList extends LightningElement {
+    searchKey;
+   tableCol=[
+        {label:'Chef', fieldName : ChefName.fieldAPIName ,type:'Text'},
+        {label:'Chef', fieldName : ChefName ,type:'Text'},
+        {label:'Menu', fieldName : ChefName ,type:'Text'}
+    ];
+    tableData;
+    itemAction;
+    prop
+
+    // @wire(getChef,{}).then(
+    //     result=>{
+    //         this.tableData = result
+    //     }
+    // ).catch(
+    //     error=>{
+    //         console.log(error);
+    //     }
+    // );
+   // @wire(getChef)tableData;
+   //@wire(getChef,{name:'$searchKey'})tableData;
+ 
+    tableData = this.Chefs;
+    searchChef(event){
+       
+        
+        this.searchKey = event.target.value;
+        this.prop = this.searchKey;
+        //@wire(getChef,{name:'$searchKey'})this.tableData;
+    }
+
+    
+   
+}
