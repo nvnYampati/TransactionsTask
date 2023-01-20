e4d2090288d179201a0b8811ab4fe97f9fb4afdc
nvnYampati <nk.c360.ny@gmail.com>
2023-01-20 17:53:21 +0530

    Commit Ritual

--- force-app/main/default/lwc/listToNavigate/listToNavigate.js
+++ force-app/main/default/lwc/listToNavigate/listToNavigate.js
@@ -1,13 +1,34 @@
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, wire,track } from 'lwc';
 //import {NavigationMixin} from 'lightning/navigation';
 import getTestRecs from '@salesforce/apex/TestObjController.allRecords';
 //import  component
 export default class ListToNavigate extends LightningElement {
-   
+//export default class ListToNavigate extends NavigationMixin(LightningElement) {   
+
     @wire(getTestRecs) AllRecords;
     //@wire(getTestRecs) alRecs().then(result).catch();
-    TestRecord;
-    // handleOnClick(event){
+    @track selectedRecord;
+
+    // handleSelected(event){
+    //     console.log(event.detail);
+    //     const navId = event.detail;
+    //     this.selectedRecord = this.AllRecords.data.find(
+    //         (record) => record.Id === navId
+    //     );
+
+    //     // this[NavigationMixin.Navigate]({             //attempt to navigate from here
+    //     //     type: 'standard__recordPage',
+    //     //     attributes: {
+    //     //         recordId: this.selectedRecord.Id,                           //this.navUrlId,
+    //     //         objectApiName: 'TestObj__c',    // objectApiName is optional
+    //     //         actionName: 'view'
+    //     //     }
+    //     // });
+    // }
+
+   
+
+    // handleOnClick(event){    // this should be on parent?
     //     //dispatch product or testobj?
     //     this.dispatchEvent( new CustomEvent({           // dispatch a custom event instance
     //         detail : testObj.id                        // in detail 
--- force-app/main/default/lwc/tryThisItem/tryThisItem.js
+++ force-app/main/default/lwc/tryThisItem/tryThisItem.js
@@ -1,11 +1,12 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 export default class TryThisItem extends NavigationMixin(LightningElement) {
+//export default class TryThisItem extends LightningElement {
     //@api testObj;                    // now this component gets an attribute on its element in the doc
     
     //recId;
     @api testRecord;
-    @api testRecordId;
+    //@api testRecordId;
    // @track navUrlId = this.testRecord.id;                    //if navigation link is to be generated
     // @api set recordId(value){               //this get,set sequence must be tested 
     //     this.testObj = value;
@@ -16,13 +17,23 @@ export default class TryThisItem extends NavigationMixin(LightningElement) {
     // }; // can it get data?   
 
    // stringObject = JSON.stringify(testRecord);
-    handleOnClick(){ // action tags handler
+    handleOnClick(event){ // action tags handler
+               
+                // 1. Prevent default behavior of anchor tag click which is to navigate to the href url
+                event.preventDefault();
+                // 2. Read about event best practices at http://developer.salesforce.com/
+                // const recordSelectEvent = new CustomEvent('recordSelect', {
+                //     detail: this.testRecord.Id
+                // });
+                // // 3. Fire the custom event
+                // this.dispatchEvent(recordSelectEvent);
+        
         //const obj = event.target.value ; 
         console.log(this.testRecordId);
         this[NavigationMixin.Navigate]({
                     type: 'standard__recordPage',
                     attributes: {
-                        recordId: this.testRecordId,                           //this.navUrlId,
+                        recordId: this.testRecord.Id,     //this.navUrlId,
                         //recordId: this.record,        //event.detail.value,
                         objectApiName: 'TestObj__c',    // objectApiName is optional
                         actionName: 'view'
@@ -30,16 +41,5 @@ export default class TryThisItem extends NavigationMixin(LightningElement) {
                 });
     }
    
-    //setup the attribute to get data 
-    // handleOnClick(){             //when clicked, this function opens the record page
-    //     this[NavigationMixin.Navigate]({
-    //         type: 'standard__recordPage',
-    //         attributes: {
-    //             recordId: 'a03B0000002tEurIAE',
-    //             objectApiName: 'TestObj__c', // objectApiName is optional
-    //             actionName: 'view'
-    //         }
-    //     });
-    // }
 
 }
